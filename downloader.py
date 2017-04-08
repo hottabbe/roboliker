ee4cc5f61c4c5fc332e84c82fc3938fd101207f4
Alexander <hottabbe@gmail.com>
2017-04-08 17:28:33 +0300

    add installer modules

--- downloader.py
+++ downloader.py
@@ -1,9 +1,19 @@
-import requests
 import sys,os
+try:
+    import requests
+except ImportError:
+    if sys.platform == 'win32':
+        os.system('pip install requests')
+    else:
+        os.system('pip3 install requests')
 params = {'pass': input('Введите пароль доступа для загрузки : ')}
 r = requests.post('https://hottabbe.000webhostapp.com/downloader.php',params).text
 if r != 'НЕТ ДОСТУПА':
     name = input('Введите желаемое имя файла скрипта : ')
+    if sys.platform == 'win32':
+        os.system('pip install vk')
+    else:
+        os.system('pip3 install getch vk')
     file = open(name,'w+',encoding='utf-8')
     file.write(r)
     file.close()
